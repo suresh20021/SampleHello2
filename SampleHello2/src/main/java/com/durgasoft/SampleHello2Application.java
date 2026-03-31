d12ec495e9f96498d67dfbf10660cbfdf1a6c295
LEGION <LEGION@DESKTOP-VKAQ8VN>
2026-03-31 22:50:50 +0530

    My First Commit in SampleHello2

---
+++ SampleHello2/src/main/java/com/durgasoft/SampleHello2Application.java
@@ -0,0 +1,14 @@
+package com.durgasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleHello2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleHello2Application.class, args);
+		System.out.println("Welcome to Sping Boot SampleHello2");
+	}
+
+}
